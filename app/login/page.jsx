fbe86168a59d4b9d273cf11c4a1eac03ca2977c7
AdamHafidz <adamjuni2004@gmail.com>
2023-12-18 13:55:47 +0700

    Register and Login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="max-w-md mx-auto px-4 w-full flex flex-col text-center">
       <p className="mt-8">FRIENDLY USE, FEELS FUN</p>
       <img className="mx-auto  my-24 w-96 h-20" src="/logo-muxtopia.png" alt="" />
-      <button className="w-full p-3 bg-primary rounded-sm mb-4">Create Account</button>
+      <input type="text" placeholder="Enter Your DID" className="p-3 mb-4 text-dark" />
       <Link className="w-full p-3 bg-primary rounded-sm" href="/login">Login</Link>
     </div>
   )
